9c56c61d49c806b5a9746f42dde77f08dac1b57d
bytedance19 <yijiubd@gmail.com>
2023-05-23 12:44:44 +0800

    feat:textblob

--- nlp/test.py
+++ nlp/test.py
@@ -1,10 +1,7 @@
-import random
-random_responses = ["That is quite interesting, please tell me more.",
-                    "I see. Do go on.",
-                    "Why do you say that?",
-                    "Funny weather we've been having, isn't it?",
-                    "Let's change the subject.",
-                    "Did you catch the game last night?"]
+from textblob import TextBlob
+from textblob.np_extractors import ConllExtractor
+# import and create a Conll extractor to use later 
+extractor = ConllExtractor()
 print("""Hello, I am Marvin, the simple robot.
 You can end this conversation at any time by typing 'bye'
 After typing each answer, press 'enter'
@@ -14,4 +11,22 @@ while True:
     if(input_str == 'bye'):
         print('Bye!')
         break
-    print(random.choice(random_responses))
+    input_blob=TextBlob(input_str, np_extractor=extractor)
+    if input_blob.polarity<=-0.5:
+        response = "Oh dear,that sounds bad. "
+    elif input_blob.polarity<=0:
+        response = "Hmm, that's not great. "
+    elif input_blob.polarity<=0.5:
+        response = "Well, that sounds positive. "
+    elif input_blob.polarity<=1:
+        response = "Wow,that sounds great. "
+    
+    #If a noun phrase is detected in the sentiment, pluralize it and ask for more input on that topic
+    if len(input_blob.noun_phrases)>0:
+        response = response + "Can you tell me more about " + input_blob.noun_phrases[0]+'s' + "?"
+    else:
+        response = response + "Can you tell me more?"
+    print(response)    
+    
+
+    
